33fb62236e39dc72dbb444dfbe3b2794fa5679a5
karl Jurvanen <karl.jurvanen@eng.tamk.fi>
2018-09-06 09:03:45 +0300

    Material UI working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,4 @@
 import * as React from "react";
-import Clock from "../src/Clock";
+import Home from "../src/Home";
 
-export default () => (
-  <div>
-    <Clock offset={0} />
-    <Clock offset={1} />
-    <Clock offset={2} />
-    <Clock offset={3} />
-    <Clock offset={4} />
-    <Clock offset={5} />
-    <Clock offset={6} />
-    <Clock offset={7} />
-    <Clock offset={8} />
-    <Clock offset={9} />
-
-  </div>
-);
+export default () => <Home />;
---
+++ src/Home.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import Clock from "../src/Clock";
+import Button from "@material-ui/core/Button";
+
+export default () => (
+  <div>
+    <Button color="primary">Material UI Test</Button>
+  </div>
+);
