0e2b8dc8ba8ad63f488b392cf5f1d24611c5ff93
karl Jurvanen <karl.jurvanen@eng.tamk.fi>
2018-09-23 15:18:43 +0300

    React 3b finished

--- src/Country.tsx
+++ src/Country.tsx
@@ -1,17 +1,16 @@
 import * as React from "react";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
 function Country(props) {
   return (
-    <Card style={{ margin: 5, width: "100%" }}>
-      <CardActionArea style={{ width: "100%" }}>
+    <Card>
+      <CardActionArea
+        onClick={() => props.selectCard()}
+        style={{ width: "100%" }}
+      >
         <CardContent>
           <Typography color="textSecondary">{props.maa.ID}</Typography>
 
@@ -33,4 +32,3 @@ function Country(props) {
 }
 
 export default Country;
-//export default withStyles(styles)(Country);
