b70768a9cf534a4701bfe4a1004ecb21e8af6915
karl Jurvanen <karl.jurvanen@eng.tamk.fi>
2018-12-04 21:07:17 +0200

    users page

---
+++ pages/users.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import Home from "../src/Home";
+import Users from '../src/Users'
+
+export default () => (
+  <Home page="Users">
+    <Users></Users>
+  </Home>
+);
