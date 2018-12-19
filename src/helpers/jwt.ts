830011263b6aca0152c39a71ebbaea129b7f9766
karl Jurvanen <karl.jurvanen@eng.tamk.fi>
2018-12-19 17:12:28 +0200

    jwt user id

--- src/helpers/jwt.ts
+++ src/helpers/jwt.ts
@@ -1,8 +1,25 @@
 import Router from "next/router";
+import jwtDecode from "jwt-decode";
+
 export const getJwt = () => {
-    return localStorage.getItem('jwt');
-}
+  try {
+    return localStorage.getItem("jwt");
+  } catch (error) {
+    return null;
+  }
+};
 
 export const reroute = (url: string) => {
-    Router.push(url);
-  };
+  const isClient = typeof document !== "undefined";
+  isClient && Router.replace(url);
+};
+
+export const getUserId = () => {
+  try {
+    const jwt = getJwt();
+    const user = jwtDecode(jwt);
+    return user.id;
+  } catch (error) {
+    return false;
+  }
+};
